724e7be5c114358567ab026b53d7865dbd781dee
sanup132 <sanup.cse@example.com>
2021-02-09 19:33:33 +0530

    notepad_files

---
+++ Sanup.java
@@ -0,0 +1,5 @@
+class Sanup{
+public static void main(String []args){
+System.out.println("my name is khan");
+}
+}
---
+++ Table.java
@@ -0,0 +1,8 @@
+Class Table {
+	pub;lic static void main(String args[]) {
+	int a = 4, c;
+	for(int i = 1, i <= 10; i++) {
+		c = a * i;
+		System.out.prinln( a + " * " + i " = " + c);
+	}
+}
